97546ce05bacbd0331047ad9fe08b634af491b94
Leonardocezary <leonardocezarychisciuc@gmail.com>
2016-05-18 00:23:12 +0300

    Contract timing

--- C4JTest/src/com/c4j/stack/StackContract.java
+++ C4JTest/src/com/c4j/stack/StackContract.java
@@ -5,7 +5,6 @@ import static de.vksi.c4j.Condition.postCondition;
 import static de.vksi.c4j.Condition.preCondition;
 import static de.vksi.c4j.Condition.result;
 import static de.vksi.c4j.Condition.old;
-
 import de.vksi.c4j.ClassInvariant;
 import de.vksi.c4j.Target;
 
@@ -22,37 +21,60 @@ public class StackContract<E> extends Stack<E> {
 
 	public StackContract() {
 		super();
-		/*if (preCondition()) {
-			assert target.getSize() == 0 : "size = 0";
-		}
-		if (postCondition()) {
-			assert target.getSize() == DEFAULT_CAPACITY : " size = MIN_CAPACITY";
-		}*/
+		/*
+		 * if (preCondition()) { assert target.getSize() == 0 : "size = 0"; } if
+		 * (postCondition()) { assert target.getSize() == DEFAULT_CAPACITY :
+		 * " size = MIN_CAPACITY"; }
+		 */
 	}
 
 	@Override
 	public void push(E element) {
 		if (preCondition()) {
+			long startTime = System.nanoTime();
+
 			assert target.getSize() >= 0 : "size >= 0";
+			
+			long endTime = System.nanoTime();
+			double x = (double) (endTime - startTime) / 1000000000.0;
+			System.out.println("\n\nPush PreCondition total execution time is: " + x + " seconds.\n");
 		}
-		
+
 		if (postCondition()) {
+			long startTime = System.nanoTime();
+			
 			assert target.getSize() == old(target.getSize()) + 1 : " balance set";
+			
+			long endTime = System.nanoTime();
+			double x = (double) (endTime - startTime) / 1000000000.0;
+			System.out.println("\n\nPost PostCondition total execution time is: " + x + " seconds.\n");
 		}
 	}
 
 	@Override
 	public E pop() {
 		if (preCondition()) {
+			long startTime = System.nanoTime();
+			
 			assert target.getSize() >= 1 : "size >= 1";
+			
+			long endTime = System.nanoTime();
+			double x = (double) (endTime - startTime) / 1000000000.0;
+			System.out.println("\n\nPop PreCondition total execution time is: " + x + " seconds.\n");
 		}
-		
+
 		if (postCondition()) {
+			long startTime = System.nanoTime();
+			
 			assert target.getSize() == old(target.getSize()) - 1 : " balance set";
 			E e = result();
-			assert e == old(target.getElements()[target.getSize() - 1]): "result == old (elements[size-1])"; 
+			assert e == old(target.getElements()[target.getSize() - 1]) : "result == old (elements[size-1])";
+			
+			long endTime = System.nanoTime();
+			double x = (double) (endTime - startTime) / 1000000000.0;
+			System.out.println("\n\nPop PostCondition total execution time is: " + x + " seconds.\n");
 		}
-		
+
 		return ignored();
 	}
 

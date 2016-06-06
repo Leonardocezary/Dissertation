b76610597ba4fda00f49030980cf4a490eaaac86
Leonardocezary_Ioan <Leonardocezary_Ioan@Leonardocezary>
2016-06-06 11:10:47 +0300

    Disable LOGGING

--- C4JTest/src/com/c4j/stack/main/Main.java
+++ C4JTest/src/com/c4j/stack/main/Main.java
@@ -5,24 +5,24 @@ import com.c4j.stack.Stack;
 public class Main {
 
 	public static void main(String[] args) {
-		  Stack<Integer> myStack = new Stack<Integer>();
+		
+		Stack<Integer> myStack = new Stack<Integer>();
 
-		  long startTime = System.nanoTime();
-		  
-		  for (int i = 0; i < 1000000; i++){
-		   myStack.push(i);
-		  }
-		  
-		  /*myStack.push(1);
-		  myStack.push(2);
-		  myStack.push(3);*/
-		  
-		  long endTime = System.nanoTime();
-		  
-		  double x = (double) (endTime - startTime)/ 1000000000.0;
-		  
-		  System.out.println("\nTotal execution time is: " + x + " seconds.");
-		  
+		long startTime = System.nanoTime();
+
+		for (int i = 0; i < 1000000; i++) {
+			myStack.push(i);
+		}
+
+		/*
+		 * myStack.push(1); myStack.push(2); myStack.push(3);
+		 */
+
+		long endTime = System.nanoTime();
+
+		double x = (double) (endTime - startTime) / 1000000000.0;
+
+		System.out.println("\nTotal execution time is: " + x + " seconds.");
 
 	}
 
